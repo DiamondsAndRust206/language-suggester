1327424085bd5ca03ea36cbd0e13f286cabdaa56
Timothy Gibson <ryaninlux@gmail.com>
2022-04-08 12:15:22 -0700

    Add document ready function to js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+
+    const fightInput = $("#fight").val();
+    const result=()
+    $("#output").text(result);
+
+
+  $("#").show();
+
+  });
+})
